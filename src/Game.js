1825afdde4d10be643e9ec253fbd1d479239cf0a
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-11-08 12:18:11 +0100

    Promote Pawns

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,48 @@
 import * as Chess from "chess.js";
 import { BehaviorSubject } from "rxjs";
 
-const chess = new Chess();
+let promotion = "rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5";
 
-export const gameSubject = new BehaviorSubject({
-  board: chess.board(),
-});
+const chess = new Chess(promotion);
 
-export function move(from, to) {
-  const legalMove = chess.move({ from, to });
-  if (legalMove) {
-    gameSubject.next({ board: chess.board() });
+export const gameSubject = new BehaviorSubject();
+
+export function initGame() {
+  updateGame();
+}
+
+export function move(from, to, promotion) {
+  let tempMove = { from, to };
+  if (promotion) {
+    tempMove.promotion = promotion;
   }
+  const legalMove = chess.move(tempMove);
+
+  if (legalMove) {
+    updateGame();
+  }
+}
+
+export function handleMove(from, to) {
+  const promotions = chess.moves({ verbose: true }).filter((m) => m.promotion);
+  if (promotions.some((p) => `${p.from}:${p.to}` === `${from}:${to}`)) {
+    const pendingPromotion = {
+      from,
+      to,
+      color: promotions[0].color,
+    };
+    updateGame(pendingPromotion);
+  }
+  const { pendingPromotion } = gameSubject.getValue();
+  if (!pendingPromotion) {
+    move(from, to);
+  }
+}
+
+function updateGame(pendingPromotion) {
+  const newGame = {
+    board: chess.board(),
+    pendingPromotion,
+  };
+  gameSubject.next(newGame);
 }
