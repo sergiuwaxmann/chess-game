9c0c60bbb0fc25ab41e7f9933e4667f3af3e2f9b
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-11-08 13:37:44 +0100

    Detect Game Over & Show Result

--- src/Game.js
+++ src/Game.js
@@ -2,12 +2,20 @@ import * as Chess from "chess.js";
 import { BehaviorSubject } from "rxjs";
 
 let promotion = "rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5";
+let staleMate = "4k3/4P3/4K3/8/8/8/8/8 b - - 0 78";
+let checkMate = "rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 3";
+let insufficientMaterial = "k7/8/n7/8/8/8/8/7K b - - 0 1";
 
-const chess = new Chess(promotion);
+const chess = new Chess(checkMate);
 
 export const gameSubject = new BehaviorSubject();
 
 export function initGame() {
+  updateGame();
+}
+
+export function resetGame() {
+  chess.reset();
   updateGame();
 }
 
@@ -40,9 +48,33 @@ export function handleMove(from, to) {
 }
 
 function updateGame(pendingPromotion) {
+  const isGameOver = chess.game_over();
+
   const newGame = {
     board: chess.board(),
     pendingPromotion,
+    isGameOver,
+    result: isGameOver ? getGameResult : null,
   };
+
   gameSubject.next(newGame);
+}
+
+function getGameResult() {
+  if (chess.in_checkmate()) {
+    const winner = chess.turn() === "w" ? "BLACK" : "WHITE";
+    return `CHECKMATE - WINNER ${winner}`;
+  } else if (chess.in_draw()) {
+    let reason = "50 MOVES RULE";
+    if (chess.in_stalemate()) {
+      reason = "STALEMATE";
+    } else if (chess.in_threefold_repetition()) {
+      reason = "REPETITION";
+    } else if (chess.insufficient_material()) {
+      reason = "INSUFFICIENT MATERIAL";
+    }
+    return `DRAW - REASON ${reason}`;
+  } else {
+    return "UNKNOWN REASON";
+  }
 }
